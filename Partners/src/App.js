ae288ecb4fcfc69af41a428a8a193f4a7cb40b76
Pooja Prashar <44187418+PoojaPrashar@users.noreply.github.com>
2020-09-02 13:44:07 -0700

    Partners Page Source Code
    This file consists of all the images, css and source code for the partners page

---
+++ Partners/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './index.css';
+import Home from './home';
+function App() {
+    return (
+        <Home />
+    );
+}
+export default App;
