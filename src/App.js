88678eebddf6a7e8fe7cd908b9bee28b0dc66132
Surya Konda <suryakonda897@gmail.com>
2020-09-14 10:06:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter} from 'react-router-dom'
 import USAMap from "react-usa-map";
 import { useState } from "react";
 import { STATEINFO } from "./states";
@@ -8,7 +9,11 @@ function TableComponent(props) {
 // const crimerate = +((props.crime / props.pop).toFixed(4) * 100).toFixed(3);
 return (
   <div>
-    <h2>{props.name}</h2>
+    <div className="d-inline" id="table__title">
+      <h2 className="d-inline" style={{color: "#32AB28"}}>{props.name}</h2>
+      <a href={`${props.report_link}`} className="d-inline" style={{color: "#5BDCDE"}}> (Click here for full report)</a>
+    </div>
+    <hr/>
         <table
                 style={{
                   width: "80%",
@@ -19,27 +24,36 @@ return (
                //backgroundColor: #5b6770,
                 }}
               >
-                <tr>
-                  <th>Population with Criminal Records</th>
-                  <th>Eligible for Expungement</th>
-                  <th>Expungement</th>
-                  <th>Expungement Gap</th>
-                  <th>Years to clear the Gap</th>
-        
-                </tr>
-                <tr>
-                  <td>{props.pop}</td>
-                  <td>{props.crime}</td>
-                  <td>{props.exp}</td>
-                  <td>{props.expgap}</td>
-                  <td>{props.yearsToClear}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                      <th>Population with Criminal Records</th>
+                    </tr>
+                    <tr><td>{props.pop}</td></tr>
+                    <tr>
+                      <th>Eligible for Expungement</th>
+                    </tr>
+                    <tr><td>{props.crime}</td></tr>
+                    <tr>
+                      <th>Expungement</th>
+                    </tr>
+                    <tr><td>{props.exp}</td></tr>
+                    <tr>
+                      <th>Expungement Gap</th>
+                    </tr>
+                    <tr><td>{props.expgap}</td></tr>
+                    <tr>
+                      <th>Years to clear the Gap</th>
+                    </tr>
+                    <tr>
+                      <td>{props.yearsToClear}</td>
+                    </tr>
+
+                </tbody>
               </table>
             </div>
           );
         }
                      function Contact() {
-                       const BASE_URL = "https://docs.google.com/document/d/11cmMQL2Dep01NQ_mtHsekvkHwxIHYQ2kxsSgtTXoNoI/edit"
                        const [showmap, setShowMap] = useState(false);
                        const [popualtion, setPopulation] = useState();
                        const [expungment, setExpungment] = useState();
@@ -47,10 +61,11 @@ return (
                        const [years, setYears] = useState();
                        const [crime, setCrime] = useState();
                        const [statename, setStateName] = useState();
+                       const [reportLink, setReportLink] = useState();
                        const mapHandler = (event) => {
                         //  const state = STATEINFO[Math.floor(Math.random() * STATEINFO.length)];
                         for(var i=0; i<STATEINFO.length;i++){
-                          if(event.target.dataset.name == STATEINFO[i].stateName){
+                          if(event.target.dataset.name === STATEINFO[i].stateName){
                             var state = STATEINFO[i]
                             setPopulation(state.Total_Population);
                             setExpungment(state.Expungement);
@@ -58,7 +73,9 @@ return (
                             setCrime(state.Totoal_Crimes);
                             setStateName(event.target.dataset.name);
                             setYears(state.Years_to_clear);
+                            setReportLink(state.report_link)
                             setShowMap(true);
+                            console.log(state.report_link)
                             break;
                           }
                           else{
@@ -80,11 +97,19 @@ return (
                        };
                        return (
                          <div align="center">
-                           <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
-                           {showmap && <a href={BASE_URL}>Link to criminal records report!</a>}
-                           {showmap && <TableComponent pop={popualtion} crime={crime} exp={expungment} expgap={expungmentGap} yearsToClear={years} name={statename} />}
+                           <BrowserRouter>
+                           <div className="row">
+                             <div className="col-8" id="map__container">
+                                <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
+                             </div>
+                             <div className="col-4" id="report__table">
+                                {showmap && <TableComponent pop={popualtion} crime={crime} exp={expungment} expgap={expungmentGap} yearsToClear={years} name={statename} report_link={reportLink}/>}
+                             </div>
+                           </div>
+                           </BrowserRouter>
                          </div>
                        );
                      }
                      export default Contact;
                     
+
